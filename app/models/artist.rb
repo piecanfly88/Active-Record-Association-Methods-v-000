c30cbb506f5808df15c970cd7c65f6cc3091ee3f
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-12-19 21:29:06 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,13 +1,9 @@
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
-  
-  def get_first_song
-
 
   def get_genre_of_first_song
-    #return the genre of the artist's first saved song
-    Genre.artist.first
+    self.songs.first.genre
   end
 
   def song_count
